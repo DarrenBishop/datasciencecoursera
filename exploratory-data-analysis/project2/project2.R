e49e0f31756344371787c1d7e3d6b8fb335bc495
Darren Bishop <mail@darrenbishop.com>
2015-10-22 06:01:14 +0100

    squash

--- exploratory-data-analysis/project2/project2.R
+++ exploratory-data-analysis/project2/project2.R
@@ -1,5 +1,7 @@
+library(dplyr)
+
 ## This first line will likely take a few seconds. Be patient!
 
-nei = readRDS("summarySCC_PM25.rds")
+nei = tbl_df(readRDS("summarySCC_PM25.rds"))
 
-scc = readRDS("Source_Classification_Code.rds")
+scc = tbl_df(readRDS("Source_Classification_Code.rds"))
