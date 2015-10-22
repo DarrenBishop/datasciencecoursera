251ea3ff19bcaf1a6a8acbc93427762b1750f5e9
Darren Bishop <mail@darrenbishop.com>
2015-10-22 07:48:54 +0100

    R script for plot1

---
+++ exploratory-data-analysis/project2/plot1.R
@@ -0,0 +1,29 @@
+library(dplyr)
+
+## This first line will likely take a few seconds. Be patient!
+
+#nei = tbl_df(readRDS("summarySCC_PM25.rds"))
+
+#scc = tbl_df(readRDS("Source_Classification_Code.rds"))
+
+#dt = nei %>% inner_join(scc, by = SCC)
+
+#dts = dt %>% group_by(year) %>% summarise(TotalPM2.5 = sum(Emissions))
+
+png("plot1.png", width = 480, height = 480)
+
+par(pin = c(5, 5), mar = c(3, 5, 3, 1))
+
+xaxis = seq(1999, 2008, by=3)
+
+ylim = with(dts, c(min(TotalPM2.5), max(TotalPM2.5))) / 1e6
+
+yaxis = 2:8
+
+plot(dts$year, dts$TotalPM2.5/1e6, type = "l", axes = F, ylab = "Total PM2.5 (millions)", col = "red")
+title("Total Emissions from PM2.5 in the United States")
+axis(side = 1, at = xaxis)
+axis(side = 2, at = yaxis)
+box()
+
+dev.off()
--- exploratory-data-analysis/project2/project2.R
+++
@@ -1,5 +0,0 @@
-## This first line will likely take a few seconds. Be patient!
-
-nei = readRDS("summarySCC_PM25.rds")
-
-scc = readRDS("Source_Classification_Code.rds")
