8505830ed87c1a95aad5f13e2093d800aa40a5c4
Darren Bishop <mail@darrenbishop.com>
2015-10-22 08:00:31 +0100

    Started R script for plot2

--- exploratory-data-analysis/project2/plot2.R
+++ exploratory-data-analysis/project2/plot2.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-readDate <- function() {
+readData <- function() {
   
   nei = tbl_df(readRDS("summarySCC_PM25.rds"))
   
@@ -10,10 +10,11 @@ readDate <- function() {
 }
 
 dts = readData() %>%
+  filter(fips == "24510") %>%
   group_by(year) %>%
   summarise(TotalPM2.5 = sum(Emissions))
 
-png("plot1.png", width = 480, height = 480)
+png("plot2.png", width = 480, height = 480)
 
 par(pin = c(5, 5), mar = c(3, 5, 3, 1))
 
@@ -21,10 +22,10 @@ xaxis = seq(1999, 2008, by=3)
 
 ylim = with(dts, c(min(TotalPM2.5), max(TotalPM2.5))) / 1e6
 
-yaxis = 2:8
+yaxis = pretty(seq(yim[1], ylim[2]))
 
 plot(dts$year, dts$TotalPM2.5/1e6, type = "l", axes = F, ylab = "Total PM2.5 (millions)", col = "red")
-title("Total Emissions from PM2.5 in the United States")
+title("Total Emissions from PM2.5 in Baltimore City, Maryland")
 axis(side = 1, at = xaxis)
 axis(side = 2, at = yaxis)
 box()
